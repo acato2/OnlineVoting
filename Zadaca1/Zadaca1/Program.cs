05e5677c981a9a55912bc0ec69eb7cab38842c50
acato2 <acato1@etf.unsa.ba>
2022-10-31 12:52:25 +0100

    Commit prazne aplikacije

---
+++ Zadaca1/Zadaca1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Zadaca1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
