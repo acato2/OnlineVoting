ecc02b2e2b3f861329aac8baa03748d9aa0bf825
acato2 <acato1@etf.unsa.ba>
2022-12-09 17:39:19 +0100

    urađeni csv testovi za funkcionalnost 4

--- Zadaca1/TestProject/UnitTest4.cs
+++ Zadaca1/TestProject/UnitTest4.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using CsvHelper;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
-using CsvHelper;
+using System.Globalization;
+using System.IO;
+using System.Linq;
 
 
 namespace TestProject
@@ -65,6 +68,55 @@ namespace TestProject
 
         #endregion
 
+
+        #region CSV Testovi
+
+        static IEnumerable<object[]> StrankeCSV
+        {
+            get
+            {
+                return UcitajPodatkeCSV();
+            }
+        }
+
+        public static IEnumerable<object[]> UcitajPodatkeCSV()
+        {
+            using (var reader = new StreamReader("podaci4.csv"))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                var rows = csv.GetRecords<dynamic>();
+                foreach (var row in rows)
+                {
+                    var values = ((IDictionary<String, Object>)row).Values;
+                    var elements = values.Select(elem => elem.ToString()).ToList();
+                    yield return new object[] { elements[0], elements[1] };
+                }
+            }
+        }
+
+        [TestMethod]
+        [DynamicData("StrankeCSV")]
+        public void Test2(string naziv, string broj_glasova)
+        {
+            List<Kandidat> lista = new List<Kandidat>();
+            lista.Add(new Kandidat("Bakir", "Izetbegović", "1"));
+            lista.Add(new Kandidat("Šemsudin", "Dedić", "2"));
+            lista.Add(new Kandidat("Sabina", "Hotić", "3"));
+
+            Stranka stranka = new Stranka(naziv, Int32.Parse(broj_glasova));
+            stranka.Rukovodstvo = lista;
+            string rezultatPovratkaFunkcije =
+                "Naziv stranke: " + naziv +
+                "\nUkupan broj glasova: 0" +
+                "\nKandidati: " +
+                "\nIdentifikacioni broj: 1" +
+                "\nIdentifikacioni broj: 2" +
+                "\nIdentifikacioni broj: 3";
+            Assert.AreEqual(rezultatPovratkaFunkcije, stranka.RezultatiRukovodstva());
+
+        }
+
+        #endregion
 
     }
 
