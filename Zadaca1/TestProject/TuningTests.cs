767191ef68ad1a1fe0b9845df537292e569a3c68
Anida Nezovic <anezovic1@etf.unsa.ba>
2023-01-07 18:39:00 +0100

    izvršen code tuning 5

--- Zadaca1/TestProject/TuningTests.cs
+++ Zadaca1/TestProject/TuningTests.cs
@@ -199,5 +199,52 @@ namespace TestProject
             int y = 0;
             Assert.IsTrue(true);
         }
+
+        [TestMethod]
+        public void TestTunning5()
+        {
+            Tunning tunning = new Tunning();
+            tunning.Opis = "Kandidat je bio ";
+            List<String> stranke = new List<String>();
+            List<String> pocetak = new List<String>();
+            List<String> kraj = new List<String>();
+
+            for (int i = 0; i < 10000; i++)
+            {
+                stranke.Add("LLL");
+                pocetak.Add("18.10.2001.");
+                kraj.Add("18.12.2001.");
+                tunning.Opis += "član stranke LLL od 18.10.2001. do 18.12.2001., ";
+            }
+
+            //sada posljednji datum nece valjati i zbog toga ce metoda vratiti false
+            tunning.Opis += "član stranke SDA od 11.10.2005. do 11.11.2005., " +
+                "član stranke SDP od 22.10.2005. do 22.11.2005., " +
+                "član stranke SNSD od 11.11.2009. do 11.10.2009., ";
+
+            stranke.Add("SDA"); stranke.Add("SDP"); stranke.Add("SNSD");
+            pocetak.Add("11.10.2005."); pocetak.Add("22.10.2005."); pocetak.Add("11.11.2009.");
+            kraj.Add("11.11.2005."); kraj.Add("22.11.2005."); kraj.Add("11.10.2009.");
+
+            for (int i = 0; i < 1000; i++)
+            {
+                stranke.Add("LLL");
+                pocetak.Add("18.10.2001.");
+                kraj.Add("18.12.2001.");
+                tunning.Opis += "član stranke LLL od 18.10.2001. do 18.12.2001., ";
+            }
+
+            int x = 0;
+
+            for (int j = 0; j <= 5000; j++)
+            {
+                //tunning.ProvjeraOpisaOriginal(stranke, pocetak, kraj);
+                tunning.ProvjeraOpisaTunning5(stranke, pocetak, kraj);
+            }
+
+            int y = 0;
+
+            Assert.IsTrue(true);
+        }
     }
 }
--- Zadaca1/Zadaca1/Tunning.cs
+++ Zadaca1/Zadaca1/Tunning.cs
@@ -779,7 +779,7 @@ public class Tunning
 			}
 		}
 
-		int x = 0;
+	
 
 		foreach (String datum in kraj)
 		{
@@ -790,223 +790,115 @@ public class Tunning
 		}
 
 		/* Slučaj 4: Provjera ispravnosti dana, mjeseca i godine */
-		/* Pretpostavljamo da se datum unosi kao 1.1.2000. */
 
-		String[] pomocniPocetak = new String[3];
-		String[] pomocniKraj = new String[3];
-		String[] pomocniPocetakA = new String[3];
-		String[] pomocniKrajA = new String[3];
-		String[] pomocniPocetakB = new String[3];
-		String[] pomocniKrajB = new String[3];
-		String[] pomocniPocetakC = new String[3];
-		String[] pomocniKrajC = new String[3];
+		
 		String trenutnaGodina = DateTime.Today.ToString("yyyy");
+		int trenutnaGodinaInt = Int16.Parse(trenutnaGodina);
+		int provjeraGodine = 0;
+
+		DateTime datumPocetka1 = new DateTime();
+		DateTime datumKraja1 = new DateTime();
+
+		DateTime datumPocetka2 = new DateTime();
+		DateTime datumKraja2 = new DateTime();
+
+		DateTime datumPocetka3 = new DateTime();
+		DateTime datumKraja3 = new DateTime();
+
+		DateTime datumPocetka4 = new DateTime();
+		DateTime datumKraja4 = new DateTime();
 
 		for (int i = 0; i < pocetak.Count() - 4; i += 4)
 		{
-			pomocniPocetak = pocetak[i].Split(".");
-			pomocniKraj = kraj[i].Split(".");
+			/* Negativnost datuma ili izlazak iz opsega datuma, provjerava već klasa DateTime, koja baca izuzetak. */
 
-			/*Provjera datum pocetak*/
+			/*===================PRVA===================*/
 
-			int pomocniPocetak0 = Int16.Parse(pomocniPocetak[0]);
-			int pomocniPocetak1 = Int16.Parse(pomocniPocetak[1]);
-			int pomocniPocetak2 = Int16.Parse(pomocniPocetak[2]);
-			int trenutnaGodinaInt = Int16.Parse(trenutnaGodina);
+			datumPocetka1 = DateTime.Parse(pocetak[i]);
+			datumKraja1 = DateTime.Parse(kraj[i]);
 
+			provjeraGodine = Int16.Parse(datumPocetka1.ToString("yyyy"));
 
-			if (pomocniPocetak0 < 1 || pomocniPocetak0 > 31
-				|| pomocniPocetak1 < 1 || pomocniPocetak1 > 12
-				|| pomocniPocetak2 < 0 || pomocniPocetak2 > trenutnaGodinaInt)
+			if (provjeraGodine > trenutnaGodinaInt)
 			{
 				return false;
 			}
 
+			provjeraGodine = Int16.Parse(datumKraja1.ToString("yyyy"));
 
-			/*Provjera datum kraj*/
-
-			int pomocniKraj0 = Int16.Parse(pomocniKraj[0]);
-			int pomocniKraj1 = Int16.Parse(pomocniKraj[1]);
-			int pomocniKraj2 = Int16.Parse(pomocniKraj[2]);
-
-			if (pomocniKraj0 < 1 || pomocniKraj0 > 31
-				|| pomocniKraj1 < 1 || pomocniKraj1 > 12
-				|| pomocniKraj2 < 0 || pomocniKraj2 > trenutnaGodinaInt)
+			if (provjeraGodine > trenutnaGodinaInt)
 			{
 				return false;
 			}
 
-			if (pomocniPocetak2 > pomocniKraj2)
-			{
-				return false;
-			}
-			else if (pomocniPocetak2 == pomocniKraj2)
-			{
-				if (pomocniPocetak1 > pomocniKraj1)
-				{
-					return false;
-				}
-				else if (pomocniPocetak1 == pomocniKraj1)
-				{
-					if (pomocniPocetak0 > pomocniKraj0)
-					{
-						return false;
-					}
-				}
-			}
-
-			pomocniPocetakA = pocetak[i + 1].Split(".");
-			pomocniKrajA = kraj[i + 1].Split(".");
-
-			/*Provjera datum pocetak*/
-
-			int pomocniPocetak0A = Int16.Parse(pomocniPocetakA[0]);
-			int pomocniPocetak1A = Int16.Parse(pomocniPocetakA[1]);
-			int pomocniPocetak2A = Int16.Parse(pomocniPocetakA[2]);
+			if (datumPocetka1 > datumKraja1) return false;
 
 
-			if (pomocniPocetak0A < 1 || pomocniPocetak0A > 31
-				|| pomocniPocetak1A < 1 || pomocniPocetak1A > 12
-				|| pomocniPocetak2A < 0 || pomocniPocetak2A > trenutnaGodinaInt)
+			/*===================DRUGA===================*/
+
+			datumPocetka2 = DateTime.Parse(pocetak[i + 1]);
+			datumKraja2 = DateTime.Parse(kraj[i + 1]);
+
+			provjeraGodine = Int16.Parse(datumPocetka2.ToString("yyyy"));
+
+			if (provjeraGodine > trenutnaGodinaInt)
 			{
 				return false;
 			}
 
+			provjeraGodine = Int16.Parse(datumKraja2.ToString("yyyy"));
 
-			/*Provjera datum kraj*/
-
-			int pomocniKraj0A = Int16.Parse(pomocniKrajA[0]);
-			int pomocniKraj1A = Int16.Parse(pomocniKrajA[1]);
-			int pomocniKraj2A = Int16.Parse(pomocniKrajA[2]);
-
-			if (pomocniKraj0A < 1 || pomocniKraj0A > 31
-				|| pomocniKraj1A < 1 || pomocniKraj1A > 12
-				|| pomocniKraj2A < 0 || pomocniKraj2A > trenutnaGodinaInt)
+			if (provjeraGodine > trenutnaGodinaInt)
 			{
 				return false;
 			}
 
-			if (pomocniPocetak2A > pomocniKraj2A)
-			{
-				return false;
-			}
-			else if (pomocniPocetak2A == pomocniKraj2A)
-			{
-				if (pomocniPocetak1A > pomocniKraj1A)
-				{
-					return false;
-				}
-				else if (pomocniPocetak1A == pomocniKraj1A)
-				{
-					if (pomocniPocetak0A > pomocniKraj0A)
-					{
-						return false;
-					}
-				}
-			}
-
-			pomocniPocetakB = pocetak[i + 2].Split(".");
-			pomocniKrajB = kraj[i + 2].Split(".");
-
-			/*Provjera datum pocetak*/
-
-			int pomocniPocetak0B = Int16.Parse(pomocniPocetakB[0]);
-			int pomocniPocetak1B = Int16.Parse(pomocniPocetakB[1]);
-			int pomocniPocetak2B = Int16.Parse(pomocniPocetakB[2]);
+			if (datumPocetka2 > datumKraja2) return false;
 
 
-			if (pomocniPocetak0B < 1 || pomocniPocetak0B > 31
-				|| pomocniPocetak1B < 1 || pomocniPocetak1B > 12
-				|| pomocniPocetak2B < 0 || pomocniPocetak2B > trenutnaGodinaInt)
+			/*===================TREĆA===================*/
+
+			datumPocetka3 = DateTime.Parse(pocetak[i + 2]);
+			datumKraja3 = DateTime.Parse(kraj[i + 2]);
+
+			provjeraGodine = Int16.Parse(datumPocetka3.ToString("yyyy"));
+
+			if (provjeraGodine > trenutnaGodinaInt)
 			{
 				return false;
 			}
 
+			provjeraGodine = Int16.Parse(datumKraja3.ToString("yyyy"));
 
-			/*Provjera datum kraj*/
-
-			int pomocniKraj0B = Int16.Parse(pomocniKrajB[0]);
-			int pomocniKraj1B = Int16.Parse(pomocniKrajB[1]);
-			int pomocniKraj2B = Int16.Parse(pomocniKrajB[2]);
-
-			if (pomocniKraj0B < 1 || pomocniKraj0B > 31
-				|| pomocniKraj1B < 1 || pomocniKraj1B > 12
-				|| pomocniKraj2B < 0 || pomocniKraj2B > trenutnaGodinaInt)
+			if (provjeraGodine > trenutnaGodinaInt)
 			{
 				return false;
 			}
 
-			if (pomocniPocetak2B > pomocniKraj2B)
-			{
-				return false;
-			}
-			else if (pomocniPocetak2B == pomocniKraj2B)
-			{
-				if (pomocniPocetak1B > pomocniKraj1B)
-				{
-					return false;
-				}
-				else if (pomocniPocetak1B == pomocniKraj1B)
-				{
-					if (pomocniPocetak0B > pomocniKraj0B)
-					{
-						return false;
-					}
-				}
-			}
+			if (datumPocetka3 > datumKraja3) return false;
 
-			pomocniPocetakC = pocetak[i + 3].Split(".");
-			pomocniKrajC = kraj[i + 3].Split(".");
+			/*===================ČETVRTA===================*/
 
-			/*Provjera datum pocetak*/
+			datumPocetka4 = DateTime.Parse(pocetak[i + 3]);
+			datumKraja4 = DateTime.Parse(kraj[i + 3]);
 
-			int pomocniPocetak0C = Int16.Parse(pomocniPocetakC[0]);
-			int pomocniPocetak1C = Int16.Parse(pomocniPocetakC[1]);
-			int pomocniPocetak2C = Int16.Parse(pomocniPocetakC[2]);
+			provjeraGodine = Int16.Parse(datumPocetka4.ToString("yyyy"));
 
-
-			if (pomocniPocetak0C < 1 || pomocniPocetak0C > 31
-				|| pomocniPocetak1C < 1 || pomocniPocetak1C > 12
-				|| pomocniPocetak2C < 0 || pomocniPocetak2C > trenutnaGodinaInt)
+			if (provjeraGodine > trenutnaGodinaInt)
 			{
 				return false;
 			}
 
+			provjeraGodine = Int16.Parse(datumKraja4.ToString("yyyy"));
 
-			/*Provjera datum kraj*/
-
-			int pomocniKraj0C = Int16.Parse(pomocniKrajC[0]);
-			int pomocniKraj1C = Int16.Parse(pomocniKrajC[1]);
-			int pomocniKraj2C = Int16.Parse(pomocniKrajC[2]);
-
-			if (pomocniKraj0C < 1 || pomocniKraj0C > 31
-				|| pomocniKraj1C < 1 || pomocniKraj1C > 12
-				|| pomocniKraj2C < 0 || pomocniKraj2C > trenutnaGodinaInt)
+			if (provjeraGodine > trenutnaGodinaInt)
 			{
 				return false;
 			}
 
-			if (pomocniPocetak2C > pomocniKraj2C)
-			{
-				return false;
-			}
-			else if (pomocniPocetak2C == pomocniKraj2C)
-			{
-				if (pomocniPocetak1C > pomocniKraj1C)
-				{
-					return false;
-				}
-				else if (pomocniPocetak1C == pomocniKraj1C)
-				{
-					if (pomocniPocetak0C > pomocniKraj0C)
-					{
-						return false;
-					}
-				}
-			}
+			if (datumPocetka4 > datumKraja4) return false;
+
 		}
-
-		int y = 0;
 
 		return true;
     }
